42dcd4e6c78d27a9a32dfa9bf4132569d6c6fee6
ReBansa <Renu_Bansal@gap.com>
2022-06-21 17:42:46 +0530

    Set basic profile

---
+++ src/data/projects.js
@@ -0,0 +1,29 @@
+import project1 from '../assets/project1.png';
+import project2 from '../assets/project2.png';
+import project3 from '../assets/project3.png';
+
+const PROJECTS = [
+    {
+        id: 1,
+        title: 'Humana',
+        description: 'Health Care & Insurance',
+        link: 'https://github.com',
+        image: project1
+    },
+    {
+        id: 2,
+        title: 'GAP',
+        description: 'Clothing Brand',
+        link: 'https://github.com',
+        image: project2
+    },
+    {
+        id: 3,
+        title: 'John Deere',
+        description: 'Agriculture',
+        link: 'https://github.com',
+        image: project3
+    }
+];
+
+export default PROJECTS;
---
+++ src/data/socialProfiles.js
@@ -0,0 +1,30 @@
+import twitterIcon from '../assets/twitter_icon.png';
+import githubIcon from '../assets/github_icon.png';
+import emailIcon from '../assets/email_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
+
+const SOCIAL_PROFILES = [
+    {
+        id: 1,
+        link: 'https://github.com',
+        image: githubIcon
+    },
+    {
+        id: 2,
+        link: 'mailto:bansal2407@gmail.com',
+        image: emailIcon
+    },
+    {
+        id: 3,
+        link: 'https://linkedin.com',
+        image: linkedinIcon
+    },
+    {
+        id: 4,
+        link: 'https://twitter.com',
+        image: twitterIcon
+    }
+
+];
+
+export default SOCIAL_PROFILES;
